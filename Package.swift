71acdc13f33d8c99cabbf4a79fa6af2046541756
TheCB4 <cavelle@thecb4.io>
2020-03-28 17:26:07 -0400

    summary: Move shellkit to semantic version instead of branch
    added: []
    changed:
      .branch to .exact("0.3.0") for ShellKit
    deprecated: []
    removed: []
    fixed: []
    security: []

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     .package(url: "https://github.com/apple/swift-argument-parser.git", .exact("0.0.4")),
     .package(url: "https://github.com/mxcl/Path.swift.git", .exact("1.0.0")),
     .package(url: "https://github.com/mxcl/Version.git", .exact("2.0.0")),
-    .package(url: "https://github.com/thecb4/ShellKit.git", .branch("2630153a"))
+    .package(url: "https://github.com/thecb4/ShellKit.git", .exact("0.3.0"))
   ],
   targets: [
     .target(
