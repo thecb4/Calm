24c18f60df80383872885e72ab0c265afdfbf836
TheCB4 <cavelle@thecb4.io>
2020-03-30 18:34:29 -0400

    summary: Bumped swift tools version to 5.2
    added: []
    changed: []
    deprecated: []
    removed: []
    fixed: []
    security: []

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -7,7 +7,7 @@ let package = Package(
   name: "Calm",
   products: [
     // Products define the executables and libraries produced by a package, and make them visible to other packages.
-    .library(name: "Calm", targets: ["Calm"]),
+    .library(name: "Calm", targets: ["Calm"])
   ],
   dependencies: [
     // Dependencies declare other packages that this package depends on.
@@ -20,16 +20,28 @@ let package = Package(
   targets: [
     .target(
       name: "Calm",
-      dependencies: ["ArgumentParser", "Path", "Version", "ShellKit"]
+      dependencies: [
+        .product(name: "ArgumentParser", package: "swift-argument-parser"),
+        .product(name: "Path", package: "Path.swift"),
+        "Version",
+        "ShellKit"
+      ]
     ),
     .target(
       name: "TestHelpers",
-      dependencies: ["ArgumentParser", "Path"],
+      dependencies: [
+        .product(name: "ArgumentParser", package: "swift-argument-parser"),
+        .product(name: "Path", package: "Path.swift")
+      ],
       path: "Tests/Helpers"
     ),
     .testTarget(
       name: "CalmTests",
-      dependencies: ["Calm", "TestHelpers", "Path"]
+      dependencies: [
+        "Calm",
+        "TestHelpers",
+        .product(name: "Path", package: "Path.swift")
+      ]
     )
   ]
 )
